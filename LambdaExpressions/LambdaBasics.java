857cb9c2d0acb69454c640b79b789b8ae0d41c5c
CodeNinja0101 <ghumre319@gmail.com>
2024-09-12 23:27:42 +0530

    code added

--- LambdaExpressions/LambdaBasics.java
+++ LambdaExpressions/LambdaBasics.java
@@ -1,8 +1,31 @@
 package LambdaExpressions;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LambdaBasics {
     public static void main(String[] args) {
+        //Lambda expression
         Runnable r = () -> System.out.println("This is Lambda Expression!");
         r.run();
+
+
+        //Lambda in Collections
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+
+        //Filtering
+        List<Integer> evenNumber = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+//                .toList();
+        System.out.println("Even Numbers: "+evenNumber);
+
+
+        //Mapping
+        List<String> numbersStrings = numbers.stream()
+                .map(n -> "numbers "+n)
+                .toList();
+        System.out.println("Number Strings: "+numbersStrings);
     }
 }
